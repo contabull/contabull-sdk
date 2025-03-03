cf405f477b3e46ce0fab4206e4c8eeeb45732a1a
Quentin <quentinmousset@gmail.com>
2025-03-03 03:14:50 +0100

    dev

--- src/resources/charges.ts
+++ src/resources/charges.ts
@@ -2,7 +2,7 @@ import type { AxiosInstance } from "axios";
 import { validateOrThrow } from '../utils/validate-or-throw';
 import { BaseResource } from "./base-resource";
 import { ChargeCreateSchema, ChargeCreateDto, ChargeCreateResponseDto } from "../dto/charges/ChargeCreateDto";
-import { ChargeGetResponseDto, ChargeGetResponseSchema } from "../dto/charges/ChargeGetDto";
+import { ChargeGetResponseDto } from "../dto/charges/ChargeGetDto";
 
 export class Charges extends BaseResource {
   constructor(client: AxiosInstance) {
