764a5e07aedfecccea3237dfe01b70a66252da82
Neogiciaa <chevalier.stephen@hotmail.com>
2025-08-21 15:48:56 +0200

    Updated account get all dto

--- src/dto/accounts/AccountGetAllDto.ts
+++ src/dto/accounts/AccountGetAllDto.ts
@@ -2,17 +2,28 @@ import { z } from "zod";
 
 export const AccountGetAllResponseSchema = z.object({
   id: z.string(),
-  label: z.string(),
+  label: z
+  .string()
+  .nullable()
+  .transform((val) => val ?? ""),
   balance: z.object({
     availableBalanceCents: z.number(),
     pendingBalanceCents: z.number(),
   }),
   number: z.string(),
   bankProvider: z.string(),
-  ispb: z.string(),
-  agency: z.string(),
+  ispb: z
+  .string()
+  .nullable()
+  .transform((val) => val ?? ""),
+  agency: z
+  .string()
+  .nullable()
+  .transform((val) => val ?? ""),
 });
 
-export type AccountGetAllResponseDto = z.infer<
-  typeof AccountGetAllResponseSchema
->;
+export type AccountGetAllItemOutputDto = z.infer<typeof AccountGetAllResponseSchema>;
+
+export const AccountGetAllOutputSchema = z.array(AccountGetAllResponseSchema);
+
+export type AccountGetAllOutputDto = z.infer<typeof AccountGetAllOutputSchema>;
